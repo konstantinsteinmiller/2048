55bddcec2fc015a90b9820486a29c94deffd1025
Konstantin Steinmiller <konstantin.steinmiller@gmail.com>
2025-03-13 19:05:08 +0100

    add github pages

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    headers: {
+      'Permissions-Policy': '',
+    },
+  },
+})
